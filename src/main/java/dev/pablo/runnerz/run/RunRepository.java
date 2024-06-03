d7d5fbe13b948b70515fb8c1b81c4e34aa15192a
Pablo Galve <107808655+PablGalveGR@users.noreply.github.com>
2024-06-03 18:34:08 +0200

    Second import correction

--- src/main/java/dev/pablo/runnerz/run/RunRepository.java
+++ src/main/java/dev/pablo/runnerz/run/RunRepository.java
@@ -3,13 +3,9 @@ package dev.pablo.runnerz.run;
 import java.util.List;
 import java.util.Optional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.Assert;
-
-import dev.pablo.runnerz.RunnerzApplication;
 
 
 @Repository
--- src/main/java/dev/pablo/runnerz/runAdhoc/RunController_Adhoc.java
+++ src/main/java/dev/pablo/runnerz/runAdhoc/RunController_Adhoc.java
@@ -2,11 +2,11 @@ package dev.pablo.runnerz.runAdhoc;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.pablo.runnerz.run.Run;
+import dev.pablo.runnerz.run.RunNotFoundException;
 import jakarta.validation.Valid;
 import java.util.List;
 import java.util.Optional;
-import run.Run;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
