2caedc6e55d79d06d76b80d8aa4225c10639f058
Pablo Galve <107808655+PablGalveGR@users.noreply.github.com>
2024-06-16 12:04:30 +0200

    Correction Update

--- src/main/java/dev/pablo/runnerz/RunnerzApplication.java
+++ src/main/java/dev/pablo/runnerz/RunnerzApplication.java
@@ -12,25 +12,30 @@ import systemMessages.WelcomeMessage;
 
 @SpringBootApplication
 public class RunnerzApplication {
-	
-	private static final Logger log = LoggerFactory.getLogger(RunnerzApplication.class);
+
+	private static final Logger log = LoggerFactory
+			.getLogger(RunnerzApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(RunnerzApplication.class, args);
 		var WelcomeMessage = new WelcomeMessage();
 		System.out.println(WelcomeMessage.getWelcomeMessage());
 	}
+
 	@Bean
-		CommandLineRunner runner(RunRepository runRepository){
-			return args -> {
-				log.info("Welcome to Runnerz");
-				/*Run run = new Run(0, "AAAAAAAA", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 20, Location.INNER);
-				Run run2 = new Run(1, "BBBBBBBBBBB", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 10, Location.INNER);
-				//runRepository.createRun(run);
-				List<Run> runs = new ArrayList<Run>();
-				runs.add(run);
-				runs.add(run2);
-				runs.add(new Run(2, "CCCCCCCCCCC", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 10, Location.INNER));
-				log.info("Created "+runRepository.createListOfRun(runs)+" runs");*/
-			}; 
-		}
+	CommandLineRunner runner(RunRepository runRepository) {
+		return args -> {
+			log.info("Welcome to Runnerz");
+			/*
+			 * Run run = new Run(0, "AAAAAAAA", LocalDateTime.now(),
+			 * LocalDateTime.now().plus(1, ChronoUnit.HOURS), 20, Location.INNER); Run run2
+			 * = new Run(1, "BBBBBBBBBBB", LocalDateTime.now(), LocalDateTime.now().plus(1,
+			 * ChronoUnit.HOURS), 10, Location.INNER); //runRepository.createRun(run);
+			 * List<Run> runs = new ArrayList<Run>(); runs.add(run); runs.add(run2);
+			 * runs.add(new Run(2, "CCCCCCCCCCC", LocalDateTime.now(),
+			 * LocalDateTime.now().plus(1, ChronoUnit.HOURS), 10, Location.INNER));
+			 * log.info("Created "+runRepository.createListOfRun(runs)+" runs");
+			 */
+		};
+	}
 }
--- src/main/java/dev/pablo/runnerz/run/RunRepository.java
+++ src/main/java/dev/pablo/runnerz/run/RunRepository.java
@@ -2,7 +2,6 @@ package dev.pablo.runnerz.run;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.Assert;
