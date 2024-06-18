4809b54aa5fa6b6d4c976ce207e1d3bb2e8ff418
Pablo Galve <107808655+PablGalveGR@users.noreply.github.com>
2024-06-18 18:31:20 +0200

    Run front table updated

--- frontend/html/js/getAllruns.js
+++ frontend/html/js/getAllruns.js
@@ -1,19 +1,30 @@
 ///Needs the to pass as a parameter an array with the name of the properties of a Json Object
 function getTableHeaders(tableHeaders) {
-    let headers = [];
-    for (let i = 0; i < tableHeaders.length; i++) {
-      let word = tableHeaders[i];
-      let firstLetter = word[0];
-      let finalWord = firstLetter.toUpperCase() + word.substr(1, word.length);
-      headers[i] = finalWord;
-      console.log(finalWord);
-    }
-    return headers
+  let headers = [];
+  for (let i = 0; i < tableHeaders.length; i++) {
+    let word = tableHeaders[i];
+    let firstLetter = word[0];
+    let finalWord = firstLetter.toUpperCase() + word.substr(1, word.length);
+    headers[i] = finalWord;
+    //console.log(finalWord);
   }
+  return headers
+}
+function addEditAndDelete(obj) {
+  obj.edit = "";
+  obj.delete = "";
+}
+
 let app = angular.module("runnerz", []);
-app.controller("getAllruns", function($scope, $http){
-    $http.get("http://127.0.0.1:8080/api/runs").then(function (response){
-        $scope.headersUpper = getTableHeaders(Object.keys((response.data[0])));
-        $scope.runs = response.data;
-    });
+app.controller("getAllruns", function ($scope, $http) {
+  $http.get("http://127.0.0.1:8080/api/runs").then(function (response) {
+    $scope.runs = response.data;
+    $scope.headersOrderBy = getTableHeaders(Object.keys((response.data[0])));
+    $scope.runs.forEach(addEditAndDelete);
+    $scope.headers = getTableHeaders(Object.keys(($scope.runs[0])));
+    //console.log($scope.runs[0]);
+  });
+  $scope.goToDetail = function(object = "Not object received"){
+    console.log("go to detail of: " + object.title + " With id: " + object.id);
+  }
 });
