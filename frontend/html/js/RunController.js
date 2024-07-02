33077cc819c769f1e0ca1cca34ea8c7a02e26f69
Pablo Galve <107808655+PablGalveGR@users.noreply.github.com>
2024-07-02 16:44:31 +0200

    Update, asyncrony problem advancements
    The id is replaced by the name only in the first line of the table.

--- frontend/html/js/RunController.js
+++ frontend/html/js/RunController.js
@@ -19,21 +19,54 @@ function addEditAndDelete(obj) {
 let app = angular.module("runnerz", []);
 app.controller("runsController", function ($scope, $http) {
   $scope.runner = {};
+  $scope.runner = {'id': '','name':''}
   $scope.runs = {};
   $scope.runsHeadersOrderBy = {};
   $scope.run = {};
-  $scope.getAllRuns = $http.get("http://127.0.0.1:8080/api/runs").then(function (response) {
-    $scope.runs = response.data;
-    $scope.runsHeadersOrderBy = getTableHeaders(Object.keys((response.data[0])));
-    $scope.runs.forEach(addEditAndDelete);
-    //Replace all runner ids with their respective names
-    $scope.runs.forEach($scope.getUserName);
-    /*
-    $scope.headers = getTableHeaders(Object.keys(($scope.runs[0])));
-    */
-    $scope.headers = Object.keys(($scope.runs[0]));
-    console.log($scope.runs[0]);
-  });
+  $scope.runs ={};
+  getAllRuns();
+  console.log($scope.runs);
+  function getAllRuns() {
+    $http.get("http://127.0.0.1:8080/api/runs").then(function (response) {
+      let runs = response.data;
+      $scope.runsHeadersOrderBy = getTableHeaders(Object.keys((response.data[0])));
+      runs.forEach(addEditAndDelete);
+      //Replace all runner ids with their respective names
+      for(let i = 0; i<runs.length; i++){
+        runs[i].runner = getUserName(runs[i]);
+      }
+      //runs.forEach(getUserName());
+      //$scope.headers = getTableHeaders(Object.keys(($scope.runs[0])));
+      $scope.headers = Object.keys((runs[0]));
+      console.log(runs[0]);
+      $scope.runs = runs;
+    });
+  }
+  function getUserName(obj) {/// Returns an Json with the User's name
+    if ($scope.runner.id != obj.runner || $scope.runner == null) {
+      $scope.runner.id = obj.runner;
+      $http.get("http://127.0.0.1:8080/api/users/name/" + $scope.runner.id).then(function (response) {
+        let runner = response.data;
+        $scope.runner.name = runner.username;
+        obj.runner = $scope.runner.name;
+        console.log("Username = " + runner.username);
+      }, function () { console.log("Error fetching runner name") });
+    }
+    else{
+      obj.runner = $scope.runner.name;
+    }
+  }
+  /*$scope.getUserName = function (obj) {/// Returns an Json with the User's name
+    if ($scope.runner.id != obj.runner || $scope.runner == null) {
+      $scope.runner.id = obj.runner;
+      $http.get("http://127.0.0.1:8080/api/users/name/" + $scope.runner.id).then(function (response) {
+        let runner = response.data;
+        $scope.runner.name = runner.username;
+        obj.runner = $scope.runner.name;
+        console.log("Username = " + runner.username);
+      }, function () { console.log("Error fetching runner name") });
+    }
+  }*/
   $scope.goToDetail = function (object = "Not object received") {
     $scope.getRun(object);
     console.log("go to detail of: " + object.title + " With id: " + object.id);
@@ -45,15 +78,5 @@ app.controller("runsController", function ($scope, $http) {
     //$scope.runHeaders = getTableHeaders(Object.keys(($scope.run)));
     console.log("Runner username set: " + $scope.run.runner);
   }
-  $scope.getUserName = function (obj) {/// Returns an Json with the User's name
-    if ($scope.runner.id != obj.runner || $scope.runner == null) {
-      $scope.runner.id = obj.runner;
-      $http.get("http://127.0.0.1:8080/api/users/name/" + $scope.runner.id).then(function (response) {
-        let runner = response.data;
-        $scope.runner.name = runner.username;
-        obj.runner = runner.name;
-        console.log("Username = " + runner.username);
-      }, function () { console.log("Error fetching runner name") });
-    }
-  }
+
 });
