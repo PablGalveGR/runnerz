2edea879f954304814d9df6bacd0fe6f152dbf68
Pablo Galve <107808655+PablGalveGR@users.noreply.github.com>
2024-07-24 16:22:50 +0200

    Add Run Started
    Get headers from the server of a run

--- frontend/html/js/RunController.js
+++ frontend/html/js/RunController.js
@@ -1,5 +1,5 @@
-///Needs the to pass as a parameter an array with the name of the properties of a Json Object
-function getTableHeaders(tableHeaders) {
+///Needs the to pass as a parameter an array of Strings
+function fomatHeaders(tableHeaders) {
   let headers = [];
   for (let i = 0; i < tableHeaders.length; i++) {
     let word = tableHeaders[i];
@@ -26,6 +26,7 @@ app.controller("runsController", function ($scope, $http, $window) {
   $scope.runs = {};
   $scope.detailRun = false;
   $scope.editRun = false;
+  $scope.addRun = false;
   $scope.users = {};
   $scope.run = {};
   $scope.runs = [];
@@ -35,22 +36,18 @@ app.controller("runsController", function ($scope, $http, $window) {
     $scope.runs = [];
     $scope.editRun = false;
     $scope.detailRun = false;
+    $scope.addRun = false;
     $scope.users = {};
     $scope.user = {};
     getAllUsers();
     getAllRuns();
   }
-  function deleteRunFromList(run){
-    console.log(typeof $scope.runs);
-    let runIndexTodelete = $scope.runs.indexOf(run);
-    $scope.runs.splice(runIndexTodelete, 1);
-  }
   function getAllRuns() {
     $http.get("http://127.0.0.1:8080/api/runs").then(function (response) {
       let runs = response.data;
-      //$scope.runsHeadersOrderBy = getTableHeaders(Object.keys((response.data[0])));
+      //$scope.runsHeadersOrderBy = fomatHeaders(Object.keys((response.data[0])));
       runs.forEach(addEditAndDelete);
-      $scope.headers = getTableHeaders(Object.keys((runs[0])));
+      $scope.headers = fomatHeaders(Object.keys((runs[0])));
       //Replace all runner ids with their respective names
       for (let i = 0; i < runs.length; i++) {
         runs[i] = getUserName(runs[i]);
@@ -60,13 +57,13 @@ app.controller("runsController", function ($scope, $http, $window) {
       }
     });
   }
-  ///Gets all the Users of the database
+  // Gets all the Users of the database
   function getAllUsers() {
     $http.get("http://127.0.0.1:8080/api/users").then(function (response) {
       $scope.users = response.data;
     });
   }
-  ///Return the object with changing the runner id by the name of the runner
+  // Return the object with changing the runner id by the name of the runner
   function getUserName(obj) {
     $http.get("http://127.0.0.1:8080/api/users/name/" + obj.runner).then(function (response) {
       let runner = response.data;
@@ -75,14 +72,16 @@ app.controller("runsController", function ($scope, $http, $window) {
     }, function () { console.log("Error fetching runner name") });
     return obj;
   }
-  ///Opens the detailed a run's detailed view
+  /*Detail Run*/
+  // Opens the detailed a run's detailed view
   $scope.goToDetail = function (object = "Not object received") {
     $scope.detailRun = true;
     $scope.editRun = false;
+    $scope.addRun = false;
     $scope.getRun(object);
     console.log("go to detail of: " + object.title + " With id: " + object.id);
   }
-  //Gets a single run
+  // Gets a single run
   $scope.getRun = function (run) {
     $scope.run = Object.assign({}, run);
     delete $scope.run.edit;
@@ -90,15 +89,47 @@ app.controller("runsController", function ($scope, $http, $window) {
     $scope.runHeaders = Object.keys(($scope.run));
     console.log("Runner username set: " + $scope.run.runner);
   }
-  /////Edit Runs
-  ///Opens a run's edit view
+  /*Add run*/
+  // Sets the form headers
+  $scope.goToAddRun = function () {
+    $scope.detailRun = false;
+    $scope.editRun = false;
+    $scope.addRun = true;
+    let addRunHeaders = Object.keys(($scope.runs[0]));
+    addRunHeaders.splice(addRunHeaders.indexOf('edit'),1);
+    addRunHeaders.splice(addRunHeaders.indexOf('delete'),1);
+    $scope.addRunHeaders = fomatHeaders(addRunHeaders);
+  }
+  $scope.getRunToAdd = function(){
+    let runToAdd = {};
+    for (property of Object.keys($scope.addRunHeaders)) {
+      let element = document.getElementsByName(property.value)[0];
+      let value = element.value;
+      runToAdd[property] = value;
+    }
+    console.log("Run to update: " + runToUpdate.id);
+    addRun(runToUpdate);
+  }
+  function addRun(run){
+    console.log("Run to add: " + run.id);
+    /*$http.put("http://127.0.0.1:8080/api/runs/update/" + run.id, run).
+      then(function () {
+        console.log("Run edited boi");
+        refreshAll();
+      }),
+      function () { console.log("Error adding run") };
+  }*/
+  }
+  /*Edit Runs*/
+  // Opens a run's edit view
   $scope.goToEdit = function (object = "Not object received") {
     $scope.detailRun = false;
     $scope.editRun = true;
+    $scope.addRun = false;
     $scope.getRun(object);
     console.log("go to edit of: " + object.title + " With id: " + object.id);
   }
-  /// Retrieves the run that is going to be updated
+  // Retrieves the run that is going to be updated
   $scope.getRunEdit = function () {
     let runToUpdate = {};
     for (property of Object.keys($scope.run)) {
@@ -119,7 +150,14 @@ app.controller("runsController", function ($scope, $http, $window) {
       }),
       function () { console.log("Error updating run") };
   }
-  ///Delete run
+  /*Delete*/
+  // Drops the selected run from the list of runs
+  function deleteRunFromList(run) {
+    console.log(typeof $scope.runs);
+    let runIndexTodelete = $scope.runs.indexOf(run);
+    $scope.runs.splice(runIndexTodelete, 1);
+  }
+  // Delete run from the DB
   $scope.gotoDelete = function (run) {
     let choice = $window.confirm("Are you sure to delete the run: id = " + run.id + " Title = " + run.title + " ?");
     if (choice == true) {
